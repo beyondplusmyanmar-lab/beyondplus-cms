7f58b90bd706a0f4491b4e395e242764240447e9
beyondplus <beyondplus@users.noreply.github.com>
2026-07-02 21:51:48 +0800

    Enhance the News & Events admin list
    Match the other list pages: titled header with description, a top-border
    divider + padding so the table no longer sits flush against the heading,
    fa-plus icon, an empty state, and a delete confirmation.

--- resources/views/bp-admin/news/index.blade.php
+++ resources/views/bp-admin/news/index.blade.php
@@ -6,22 +6,23 @@
     <div class="row">
         <div class="col-md-12 tile">
             <div class="box box-danger">
-                <div class="box-header">
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h4>Show</h4>
+                <div class="box-header" style="padding-bottom:.75rem;">
+                    <div class="row align-items-center">
+                        <div class="col-sm-8">
+                            <h4 class="mb-0">News &amp; events</h4>
+                            <small class="text-muted">Time-sensitive announcements and events.</small>
                         </div>
-                        <div class="col-sm-3 pull-right">
+                        <div class="col-sm-4 pull-right">
                             <a href="{{ url('bp-admin/news/create') }}" class="btn btn-success  pull-right">
-                                <i class="fa fa-user-plus"></i>
-                                New
+                                <i class="fa fa-plus"></i>
+                                New item
                             </a>
                         </div>
                     </div>
                 </div>
                 <!-- /.box-header -->
-                <div class="box-body">
-                    <table  class="table table-bordered table-hover">
+                <div class="box-body pt-3" style="border-top: 1px solid #eef0f3;">
+                    <table  class="table table-hover mb-0">
                         <thead>
                         <tr>
                             <th>Name</th>
@@ -31,7 +32,7 @@
                         </tr>
                         </thead>
                         <tbody>
-                        @foreach ($post as $c)
+                        @forelse ($post as $c)
                         <tr>
                             <td>
                                 <a href="{{ url('bp-admin/news/'.$c->id.'/edit') }}" >{{$c->title}}</a> <br>
@@ -53,10 +54,12 @@
                             </td>
                             <td>
                                 <a href="{{ url('bp-admin/news/'.$c->id.'/edit') }}" class="btn btn-xs btn-info">Edit</a>
-                                    <a href="{{ url('bp-admin/news/delete', [$c->id]) }}" class="btn btn-delete btn-xs btn-danger">Delete</a>
+                                    <a href="{{ url('bp-admin/news/delete', [$c->id]) }}" class="btn btn-delete btn-xs btn-danger" onclick="return confirm('Delete this item?')">Delete</a>
                             </td>
                         </tr>
-                        @endforeach
+                        @empty
+                        <tr><td colspan="4" class="text-center text-muted py-4">No news or events yet.</td></tr>
+                        @endforelse
                         </tbody>
                     </table>
                     <div class="row">
